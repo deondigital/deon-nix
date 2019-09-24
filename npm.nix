fa3ae5481e39146aec176e07973c1bbe97da9449
Bjørn Bugge Grathwohl <bjorn.bugge@deondigital.com>
2019-09-24 20:18:11 +0200

    Install NPM manually by unpacking tarball

--- npm.nix
+++ npm.nix
@@ -4,12 +4,16 @@ pkgs.stdenv.mkDerivation rec {
   name = "npm";
   version = "6.11.3";
   propagatedBuildInputs = [ pkgs.nodejs ];
-  unpackPhase = "true"; # No sources
-  configurePhase = ''
-    mkdir -p $out
-    export HOME=$TMP
-    npm config set prefix $out
-  '';
+  src = builtins.fetchTarball {
+    url = "https://github.com/npm/cli/archive/v${version}.tar.gz";
+    sha256 = "0hwmwawdcdqx1fs03pd4lzjjr3l626fj6drcf7qg3p3ivqm9i181";
+  };
   dontBuild = true;
-  installPhase = "npm install npm@${version} -g";
+  installPhase = ''
+    mkdir $out
+    cp -R bin lib node_modules package.json package-lock.json $out/.
+    rm $out/bin/{npm,npx}
+    ln -s $out/bin/npm-cli.js $out/bin/npm
+    ln -s $out/bin/npx-cli.js $out/bin/npx
+  '';
 }
